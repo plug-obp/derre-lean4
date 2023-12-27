3189b45bb37e87232d785e79920928d2235b23da
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2023-12-27 15:07:12 +0100

    semiring and kleene algebra instances

--- RegEx/RegEx/denotational.lean
+++ RegEx/RegEx/denotational.lean
@@ -1,5 +1,15 @@
 import Mathlib.Init.Data.Nat.Notation
 import Mathlib.Init.Set
+import Mathlib.Data.Set.NAry
+import Mathlib.Data.Set.Lattice
+import Mathlib.Algebra.Ring.Basic
+
+import Mathlib.Algebra.Order.Kleene
+-- import Mathlib.Algebra.Ring.Hom.Defs
+-- import Mathlib.Data.List.Join
+-- import Mathlib.Data.Set.Lattice
+import Mathlib.Tactic.DeriveFintype
+
 import Mathlib.Tactic.Basic --for Type*
 import Mathlib.Tactic.Tauto
 import Mathlib.Tactic.PushNeg
@@ -10,6 +20,15 @@ import Mathlib.Tactic.LibrarySearch
 
 - [Regular-expression derivatives reexamined](https://www.khoury.northeastern.edu/home/turon/re-deriv.pdf)
 - [regex-derivative @ Tej Chajed](https://github.com/tchajed/regex-derivative/blob/master/regex.v)
+
+- Matlib.Computability.Language
+  A language is a Kleene Algebra:
+    an idempotent semiring with a star operation.
+-/
+
+/-!
+  # Standard names
+  These simple definitions give standard names from the theory of computation.
 -/
 
 -- The 𝒜lphabet
@@ -17,18 +36,15 @@ variable
   { 𝒜 : Type* } -- 𝒜 because we cannot use Σ
 
 /-!
-  # Standard names
-  These simple definitions give standard names from the theory of computation to
-  the analogues in this setting.
--/
-
-/-!
   A word takes elements from the (arbitrary) alphabet above. *)
   **TODO**: I think that list is too restrictive,
     we need LinearOrder here or something similar
-  Basically a word should be a linear order on a subset of 𝒜
+  Basically a word is a linear order on a subset of 𝒜
     LinearOrder X, where X ⊆ 𝒜
-  Since 𝒜 is a type, X might be a subtype
+  Since 𝒜 is a type, X might be a subtype.
+  List 𝒜 is just a particular case, where in fact we want 𝒞 = (ℕ, 𝒜).
+  For instance a word is: (1, a), (2, b), (3, c), (4, a), (5, b), (6, c)
+  where the linear order is given by ℕ, and 𝒜 is the alphabet.
 -/
 alias Word := List
 instance: Append (Word 𝒜) := ⟨ List.append ⟩
@@ -64,6 +80,7 @@ def Language 𝒜 := Set $ Word 𝒜
 instance: Membership (Word 𝒜) (Language 𝒜) := ⟨Set.Mem⟩
 instance: EmptyCollection (Language 𝒜) := ⟨ λ _ => False ⟩
 instance: Union (Language 𝒜) := ⟨Set.union⟩
+instance: Insert (Word 𝒜) (Language 𝒜) := ⟨Set.insert⟩
 
 def singleWord (w: Word 𝒜) : Language 𝒜 := {b : Word 𝒜 | b = w}
 instance: Singleton (Word 𝒜) (Language 𝒜) := ⟨singleWord⟩
@@ -71,33 +88,220 @@ def singleLetter[Singleton (Word 𝒜) (Language 𝒜)] (w: 𝒜) : Language �
 instance [Singleton (Word 𝒜) (Language 𝒜)]: Singleton 𝒜 (Language 𝒜) := ⟨singleLetter⟩
 instance: HasSubset $ Language 𝒜 := ⟨Set.Subset⟩
 
+@[ext]
+theorem ext {l m : Language 𝒜} (h : ∀ (x : Word 𝒜), x ∈ l ↔ x ∈ m) : l = m :=
+  Set.ext h
+
 def concatenationL(L₁ L₂: Language 𝒜): Language 𝒜 :=
-  { w | ∀ w₁ w₂, w₁ ∈ L₁ ∧ w₂ ∈ L₂ ∧ w = w₁ ++ w₂ }
+  Set.image2 (. ++ .) L₁ L₂
 instance: Append (Language 𝒜) := ⟨concatenationL⟩
+
+lemma concat_nil: ∀ L: Language 𝒜, L ++ {} = {} := by {
+  intro L
+  apply Set.image2_empty_right
+}
+
+lemma nil_concat: ∀ L: Language 𝒜, {} ++ L = {} := by {
+  intro L
+  apply Set.image2_empty_left
+}
+
+lemma concat_empty_right (L: Language 𝒜): L ++ {[]} = L := by {
+  sorry
+}
+
+lemma concat_empty_left (L: Language 𝒜): {[]} ++ L = L := by {
+  sorry
+}
+
+instance Language.toCompleteAtomicBooleanAlgebra: CompleteAtomicBooleanAlgebra (Language 𝒜) := inferInstanceAs (CompleteAtomicBooleanAlgebra (Set (Word 𝒜)))
+
+instance Language.zero: Zero (Language 𝒜) := ⟨ ∅ ⟩
+instance Language.one: One (Language 𝒜) := ⟨ { [] } ⟩
+instance Language.inhabited: Inhabited (Language 𝒜) := ⟨ ∅ ⟩
+instance Language.add: Add (Language 𝒜) := ⟨ Set.union ⟩
+instance Language.mul: Mul (Language 𝒜) := ⟨ concatenationL ⟩
+
+theorem zero_def : (0 : Language α) = (∅ : Set _) :=
+  rfl
+
+theorem one_def : (1 : Language α) = ({[]} : Set (List α)) :=
+  rfl
+
+theorem add_def (l m : Language α) : l + m = (l ∪ m : Set (List α)) :=
+  rfl
+
+theorem mul_def (l m : Language α) : l * m = Set.image2 (· ++ ·) l m :=
+  rfl
+
+@[simp]
+theorem not_mem_zero (x : List α) : x ∉ (0 : Language α) :=
+  id
+
+@[simp]
+theorem mem_one (x : List α) : x ∈ (1 : Language α) ↔ x = [] := by rfl
+
+instance Language.toSemiring : Semiring (Language 𝒜) where
+  add := (· + ·)
+  add_assoc := Set.union_assoc
+  zero := 0
+  zero_add := Set.empty_union
+  add_zero := Set.union_empty
+  add_comm := Set.union_comm
+  mul := (· * ·)
+  mul_assoc _ _ _ := Set.image2_assoc List.append_assoc
+  zero_mul _ := Set.image2_empty_left
+  mul_zero _ := Set.image2_empty_right
+  one := 1
+  one_mul l := by {
+    simp [mul_def, one_def, Set.image]
+    conv in ([] ++ _) => rw [List.nil_append]
+    simp [Set.image]
+  }
+  mul_one l := by {
+    simp [mul_def, one_def, Set.image]
+    conv in (_ ++ []) => rw [List.append_nil]
+    simp [Set.image]
+  }
+  natCast n := if n = 0 then 0 else 1
+  natCast_zero := rfl
+  natCast_succ n := by cases n <;> simp [Nat.cast, add_def, zero_def]
+  left_distrib _ _ _ := Set.image2_union_right
+  right_distrib _ _ _ := Set.image2_union_left
 
 /-
 If L is a formal language, then Lⁱ, the iᵗʰ power of L, is the concatenation of L with itself i times.
 That is, Lⁱ can be understood to be the set of all strings that can be represented as the concatenation of i strings in L.
 -/
-def powL (L: Language 𝒜): ℕ → Language 𝒜
-| 0 => { [] }
-| (n+1) => L ++ (powL L n)
-instance: HPow (Language 𝒜) ℕ (Language 𝒜) := ⟨powL⟩
+-- def powL (L: Language 𝒜): ℕ → Language 𝒜
+-- | 0 => { [] }
+-- | (n+1) => L * (powL L n)
+-- instance: HPow (Language 𝒜) ℕ (Language 𝒜) := ⟨powL⟩
+
+
+
+@[simp]
+lemma powL_zero (L: Language 𝒜): L ^ 0 = 1 := rfl
+
+
+lemma powL_n (L: Language 𝒜): L ^ (n+1) = L * (L ^ n) := by {
+  sorry
+}
+
+lemma powL_one (L: Language 𝒜): L ^ 1 = L := by {
+  simp [powL_n]
+}
 
 /-
 The free monoid L^* is called the "Kleene star of A". Also known as Kleene closure.
 -/
 def kleene_closure(L: Language 𝒜): Language 𝒜 :=
-  { w | ∀ n: ℕ, w ∈ (L ^ n)}
-postfix:65   "⊛"    => kleene_closure
-@[simp]
-lemma kleene_closure_idempotent (L: Language 𝒜): L⊛⊛ = L⊛ := by {
-  apply Set.ext
-  intro w
-  sorry
+  { w | ∃ n: ℕ, w ∈ (L ^ n)}
+instance Language.kstar: KStar (Language 𝒜) := ⟨kleene_closure⟩
+postfix:1024 "∗" => KStar.kstar
+
+lemma kleene_closure_def(L: Language 𝒜): L∗ = { w | ∃ n: ℕ, w ∈ (L ^ n)} := rfl
+
+lemma one_in_l_star: ∀ L: Language 𝒜, 1 ⊆ L∗ := λ L w hw ↦ by { exists 0 }
+
+theorem mem_iSup {ι : Sort v} {L : ι → Language 𝒜} {x: Word 𝒜} : (x ∈ ⨆ i, L i) ↔ ∃ i, x ∈ L i :=
+  Set.mem_iUnion
+
+theorem mem_kstar(L: Language 𝒜): w ∈ L∗ ↔ ∃ n: ℕ, w ∈ (L ^ n) := by {
+  constructor <;> (rintro ⟨n, hw⟩; exact ⟨n, hw⟩)
 }
 
-def positive_closure(L: Language 𝒜): Language 𝒜 := L ++ (L⊛)
+theorem kstar_eq_iSup_pow (l : Language α) : l∗ = ⨆ i : ℕ, l ^ i := by
+  ext x
+  simp only [mem_iSup, mem_kstar]
+
+theorem iSup_mul {ι : Sort v} (l : ι → Language 𝒜) (m : Language 𝒜) :
+    (⨆ i, l i) * m = ⨆ i, l i * m :=
+  Set.image2_iUnion_left _ _ _
+
+theorem mul_iSup {ι : Sort v} (l : ι → Language 𝒜) (m : Language 𝒜) :
+    (m * ⨆ i, l i) = ⨆ i, m * l i :=
+  Set.image2_iUnion_right _ _ _
+
+theorem le_mul_congr {l₁ l₂ m₁ m₂ : Language 𝒜} : l₁ ≤ m₁ → l₂ ≤ m₂ → l₁ * l₂ ≤ m₁ * m₂ := by
+  intro h₁ h₂ x hx
+  simp only [mul_def, exists_and_left, Set.mem_image2, Set.image_prod] at hx ⊢
+  tauto
+
+@[simp]
+theorem one_add_self_mul_kstar_eq_kstar (l : Language 𝒜) : 1 + l * l∗ = l∗ := by
+  simp only [kstar_eq_iSup_pow, mul_iSup, ← pow_succ, ← pow_zero l]
+  exact sup_iSup_nat_succ _
+
+theorem mul_self_kstar_comm (l : Language α) : l∗ * l = l * l∗ := by
+  simp only [kstar_eq_iSup_pow, mul_iSup, iSup_mul, ← pow_succ, ← pow_succ']
+
+@[simp]
+theorem one_add_kstar_mul_self_eq_kstar (l : Language α) : 1 + l∗ * l = l∗ := by
+  rw [mul_self_kstar_comm, one_add_self_mul_kstar_eq_kstar]
+
+noncomputable instance Language.toKleeneAlgebra: KleeneAlgebra (Language 𝒜) :=
+  { Language.toSemiring, Language.toCompleteAtomicBooleanAlgebra, Language.kstar with
+    one_le_kstar := λ L w hw ↦ (by exists 0),
+    mul_kstar_le_kstar := fun L ↦ (one_add_self_mul_kstar_eq_kstar L).le.trans' le_sup_right,
+    kstar_mul_le_kstar := fun L ↦ (one_add_kstar_mul_self_eq_kstar L).le.trans' le_sup_right,
+    kstar_mul_le_self := fun l m h ↦ by
+      rw [kstar_eq_iSup_pow, iSup_mul]
+      refine' iSup_le (fun n ↦ _)
+      induction' n with n ih
+      · simp
+      rw [pow_succ', mul_assoc (l^n) l m]
+      exact le_trans (le_mul_congr le_rfl h) ih,
+    mul_kstar_le_self := fun l m h ↦ by
+      rw [kstar_eq_iSup_pow, mul_iSup]
+      refine' iSup_le (fun n ↦ _)
+      induction' n with n ih
+      · simp
+      rw [pow_succ, ← mul_assoc m l (l^n)]
+      exact le_trans (le_mul_congr h le_rfl) ih }
+
+lemma zero_in_l_star: ∀ L: Language 𝒜, { [] } ⊆ L∗ := by {
+  intro L
+  simp [kleene_closure_def]
+  intro w
+  intro w_in_empty
+  exists 0
+}
+
+theorem nil_mem_star (l : Language α) : [] ∈ l∗ := by {
+  simp [kleene_closure_def]
+  exists 0
+}
+
+lemma l_mem_l_star (L: Language 𝒜): ∀ w: Word 𝒜, w ∈ L → w ∈ L∗ := by {
+  intro w
+  intro w_in_L
+  exists 1
+  rw [powL_one]
+  exact w_in_L
+}
+
+lemma star_to_star_star (L: Language 𝒜): w ∈ L∗ → w ∈ L∗∗ := by simp [kstar_mul_kstar]
+
+lemma star_in_star_star (L: Language 𝒜): L∗ ⊆ L∗∗ := by {
+  simp [kstar_mul_kstar]
+  rintro ⟨⟩ <;> simp
+}
+
+lemma star_star_to_star (L: Language 𝒜): w ∈ L∗∗ → w ∈ L∗ := by simp [kstar_mul_kstar]
+
+lemma star_star_in_star(L: Language 𝒜): L∗∗ ⊆ L∗ := by {
+  simp [kstar_mul_kstar]
+  rintro ⟨⟩ <;> simp
+}
+
+@[simp]
+theorem kleene_closure_idempotent: ∀ L: Language 𝒜, L∗∗ = L∗ := λ _ ↦ by apply kstar_idem
+
+@[simp]
+lemma concat_kleene_closure_idem (L: Language 𝒜): L∗ * L∗ = L∗  := by apply kstar_mul_kstar
+
+def positive_closure(L: Language 𝒜): Language 𝒜 := L ++ (L∗)
 postfix:65   "⊕"    => positive_closure
 
 def sigma (𝒜: Type*): Language 𝒜 := { [a] | a : 𝒜 }
@@ -123,8 +327,8 @@ lemma empty_pow: ∀ n: ℕ, (∅: Language 𝒜) ^ n = {} := by {
 }
 
 @[simp]
-lemma empty_star_is_ε: (∅: Language 𝒜)⊛ = Lε := by {
-  sorry
+lemma empty_star_is_ε: (∅: Language 𝒜)∗ = Lε := by {
+  apply kstar_zero
 }
 
 @[simp]
@@ -142,9 +346,8 @@ lemma ε_pow: ∀ n: ℕ, (Lε: Language 𝒜) ^ n = Lε := by {
 }
 
 @[simp]
-lemma ε_star: (Lε: Language 𝒜)⊛ = Lε := by {
-  simp [kleene_closure, ε_pow]
-  rfl
+lemma ε_star: (Lε: Language 𝒜)∗ = Lε := by {
+  apply kstar_one
 }
 
 @[simp]
