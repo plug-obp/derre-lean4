8b1473f321532807479289a7f36ebbb886261e18
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2023-12-29 16:53:30 +0100

    some more lemmas about derivatives

--- RegEx/RegEx/language.lean
+++ RegEx/RegEx/language.lean
@@ -380,14 +380,56 @@ The derivative of a language L wrt a character c is the set of all words w for w
 -/
 def DerL (c: 𝒜) (L: Language 𝒜) : Language 𝒜 := { w | (c :: w) ∈ L }
 
+lemma DerL_def (c: 𝒜) (L: Language 𝒜) : DerL c L = { w | (c :: w) ∈ L } := rfl
+lemma DerL_empty (c: 𝒜) : DerL c ∅ = ∅ := by {
+  simp [DerL_def]
+  rfl
+}
+lemma DerL_epsilon (c: 𝒜) : DerL c Lε = ∅ := by {
+  ext w₁
+  constructor <;> (intro _; contradiction)
+}
+
+lemma DerL_singleton_eq(c: 𝒜):
+  DerL c {[c]} = Lε := by {
+  ext w₁
+  simp [DerL_def, Lε]
+  constructor
+  . intro H
+    cases w₁ with
+    | nil => rfl
+    | cons h t =>
+      exfalso
+      have hx: {w: Word 𝒜 | (c :: w) ∈ ( {[c]}: Language 𝒜)} = Lε := by sorry
+      have ht: h :: t ∈ Lε := by rw [←hx]; exact H
+      contradiction
+  . intro H
+    tauto
+}
+
+lemma DerL_singleton_neq(c: 𝒜) (d: 𝒜)(hne: c ≠ d) :
+  DerL c {[d]} = ∅ := by {
+  ext w₁
+  simp [DerL_def]
+  constructor
+  . intro H
+    have hx: {w: Word 𝒜 | (c :: w) ∈ ( {[d]}: Language 𝒜)} = ∅ := by sorry
+    rw [←hx]; exact H
+  . intro H
+    contradiction
+}
+
+lemma DerL_singleton(c: 𝒜) (d: 𝒜)[hdeq: Decidable (c = d)] :
+  DerL c {[d]} = (if c = d then Lε else ∅) := by {
+  ext w₁
+  split
+  next cd => simp [cd, DerL_singleton_eq]
+  next cnd => simp [DerL_singleton_neq _ _ cnd]
+}
+
 lemma der_head_single(w: Word 𝒜): c = x → w ∈ DerL c {[x]} → w = [] := by {
   intro H Hw
-  simp [DerL] at *
-  cases w with
-  | nil =>
-    simp [DerL]
-  | cons h t =>
-    exfalso
-    rw [H] at Hw
-    sorry
+  rw [H] at Hw
+  simp [DerL_singleton_eq] at *
+  exact Hw
 }
